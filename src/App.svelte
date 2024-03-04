28d6532d3604f3b77d8ecca02a68514c0192f6c2
Sudo-Self <sudo-self@proton.me>
2024-03-04 11:18:34 -0700

    Update App.svelte
    Signed-off-by: Sudo-Self <sudo-self@proton.me>

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,7 @@
   import { writable } from 'svelte/store';
   const messageCount = writable(0);
   let messageText = '';
-  let messages = ["love this!", "turn it up", "woah" "ayeeee"]; 
+  let messages = ["love this!", "turn it up", "woah" "ayeeee" "hot🔥"]; 
   let isDarkMode = true; 
   let isLoading = false;
 
